f73f96ad65e1826c89aed44212a34c124a734870
Akash Chaurasia <akashc@stanford.edu>
2022-11-24 20:24:03 -0800

    Much faster, at least on Myth

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -20,70 +20,44 @@
 void pageRank(Graph g, double* solution, double damping, double convergence)
 {
 
-
-  // initialize vertex weights to uniform probability. Double
-  // precision scores are used to avoid underflow for large graphs
-
     int numNodes = num_nodes(g);
     double equal_prob = 1.0 / numNodes;
 
-  /*
-     CS149 students: Implement the page rank algorithm here.  You
-     are expected to parallelize the algorithm using openMP.  Your
-     solution may need to allocate (and free) temporary arrays.
-
-     Basic page rank pseudocode is provided below to get you started:
-
-     // initialization: see example code above
-     score_old[vi] = 1/numNodes;
-
-     while (!converged) {
-
-       // compute score_new[vi] for all nodes vi:
-       score_new[vi] = sum over all nodes vj reachable from incoming edges
-                          { score_old[vj] / number of edges leaving vj  }
-       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
-
-       score_new[vi] += sum over all nodes v in graph with no outgoing edges
-                          { damping * score_old[v] / numNodes }
-
-       // compute how much per-node scores have changed
-       // quit once algorithm has converged
-
-       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
-       converged = (global_diff < convergence)
-     }
-
-   */
     bool converged = false;
     double* scores1 = (double*) malloc(sizeof(double) * numNodes);
     double* scores2 = (double*) malloc(sizeof(double) * numNodes);
-    double* score_diffs = (double*) malloc(sizeof(double) * numNodes);
 
-    std::vector<Vertex> no_outgoing;
-    #pragma omp parallel for schedule(dynamic, 3u)
-    for (int i = 0; i < numNodes; i++) {
-        if (!outgoing_size(g, i)) {
-            #pragma omp critical
-            {
-                no_outgoing.push_back(i);
-            }
-        }
-    }
-
-    #pragma omp parallel for schedule(dynamic, 32)
+    Vertex* no_outgoing_vtx = (Vertex*) malloc(sizeof(Vertex) * numNodes);
+    int no_outgoing_count{0};
+    #pragma omp parallel for
     for (int i = 0; i < numNodes; i++) {
         scores1[i] = equal_prob;
-        score_diffs[i] = 0;
+
+        if (!outgoing_size(g, i)) {
+            int idx;
+            #pragma omp critical
+            {
+                idx = no_outgoing_count++;
+            }
+            no_outgoing_vtx[idx] = i;
+        }
     }
 
     double global_diff;
     while (!converged) {
-        global_diff = 0;
 
-        #pragma omp parallel for schedule(dynamic, 32)
+        // accumulate scores for vertices with no outgoing edges
+        double no_outgoing_total_score{0};
+        #pragma omp parallel for reduction(+:no_outgoing_total_score)
+        for (int i = 0; i < no_outgoing_count; i++) {
+            no_outgoing_total_score += scores1[no_outgoing_vtx[i]];
+        }
+
+        no_outgoing_total_score = damping * no_outgoing_total_score / numNodes;
+
+        #pragma omp parallel for
         for (int i = 0; i < numNodes; i++) {
-            double new_score = 0;
+            double new_score{0};
 
             if (incoming_size(g, i)) {
                 // sum over all nodes vj reachable from incoming edges
@@ -94,33 +68,27 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
                 }
             }
 
-            new_score = (damping * new_score) + (1 - damping) / numNodes;
-
-            // sum over all nodes v in graph with no outgoing edges
-            for (int j = 0; j < no_outgoing.size(); j++) {
-                new_score += damping * scores1[no_outgoing[j]] / numNodes;
-            }
-
-            scores2[i] = new_score;
-            score_diffs[i] = abs(new_score - scores1[i]);
+            scores2[i] = no_outgoing_total_score + (damping * new_score) + (1 - damping) / numNodes;
         }
 
-        // accumulate diffs
-        #pragma omp parallel for reduction(+:global_diff) schedule(dynamic, 32)
+        // accumulate global diff, update old scores
+        global_diff = 0;
+        #pragma omp parallel for reduction(+:global_diff)
         for (int i = 0; i < numNodes; i++) {
-            global_diff += score_diffs[i];
+            global_diff += abs(scores2[i] - scores1[i]);
             scores1[i] = scores2[i];
         }
 
         converged = global_diff < convergence;
     }
 
-    #pragma omp parallel for schedule(dynamic, 32)
+    // write back solution
+    #pragma omp parallel for
     for (int i = 0; i < numNodes; i++) {
         solution[i] = scores1[i];
     }
 
     free(scores1);
     free(scores2);
-    free(score_diffs);
+    free(no_outgoing_vtx);
 }
